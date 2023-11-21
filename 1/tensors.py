ece0eb6438cd9d071184a95830346ce9b4abf1a2
zhangyuhe <862000954@qq.com>
2023-11-21 17:41:55 +0800

    添加加法操作

--- 1/tensors.py
+++ 1/tensors.py
@@ -21,3 +21,27 @@ print(x)
 # 创建一个tensor 基于已经存在的tensor
 x = x.new_ones(5, 3, dtype=torch.double)
 print(x)
+
+x = torch.randn_like(x, dtype=torch.float)
+print(x)
+
+# 获取它的维度信息
+print(x.size())  # torch.Size([5, 3]) torch.Size是一个元组， 支持左右的元组操作
+
+# 加法 1
+y = torch.rand(5, 3)
+print(y)
+print(x + y)
+
+# 加法 2
+print(torch.add(x, y))
+
+# 加法 提供一个tensor作为参数
+result = torch.empty(5, 3)
+torch.add(x, y, out=result)
+print(result)
+
+# 加法 3 in-place
+y.add_(x)
+print(y)
+
